fcbbef853d40c58f45f90a1670a90424dc344f84
overroading <overroading@gmail.com>
2023-09-16 20:08:06 +0900

    230916

---
+++ ch02/ch02_3/src/App.tsx
@@ -0,0 +1,19 @@
+import { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    const isLoading = true
+    if(isLoading){
+      return <p>loading...</p>
+    }
+    return (
+      <ul>
+        <li>
+          <a href="http://www.google.com">
+            <p>go to Google</p>
+          </a>
+        </li>
+      </ul>
+    )
+  }
+}
